91adf41166c1a055bece8c3aa3d4ca8796738233
jiangxinyu11 <15146166444@163.com>
2017-02-10 22:47:37 +0800

    css

--- jQ/jQuery练习/1_表单/2-多行文本框高度变化/demo1_1.js
+++ jQ/jQuery练习/1_表单/2-多行文本框高度变化/demo1_1.js
@@ -4,12 +4,19 @@
 $(function () {
     var $comment = $('#comment');
     $('.bigger').on('click',function () {
-        if($comment.height()<500){
-
-            $comment.height($comment.height()+50);
-        }else{
-
-        }
+        // if($comment.height()<500){
+        //
+        //     $comment.height($comment.height()+50);
+        // }
+        $comment.height($comment.height()<500?$comment.height()+50:$comment.height());
+        // console.log($comment.height());
+    });
+    $('.smaller').on('click',function () {
+        // if($comment.height()>50){
+        //
+        //     $comment.height($comment.height()-50);
+        // }
+        $comment.height($comment.height()>50?$comment.height()-50:$comment.height());
         // console.log($comment.height());
     })
 });
