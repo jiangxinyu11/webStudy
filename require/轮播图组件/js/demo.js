3a5b321880d6356b1ee76e5772adbc9fabc937a3
jiangxinyu11 <15146166444@163.com>
2017-02-22 11:13:18 +0800

    css

--- require/轮播图组件/js/demo.js
+++ require/轮播图组件/js/demo.js
@@ -1,7 +1,7 @@
 /**
  * Created by SAMSUNG on 2017/2/20.
  */
-require(["jquery","banner"],function ($,Banner) {
+require(["jquery","banner"],function ($, Banner) {
     $('#button').on('click',function () {
 
         var banner = new Banner();
