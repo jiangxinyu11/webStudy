0e3ccb8e2c2ff8f45368e2b1585cbbf1c4df5593
jiangxinyu11 <15146166444@163.com>
2017-03-14 21:06:42 +0800

    css

---
+++ gulp/gulpfile.js
@@ -0,0 +1,14 @@
+var gulp = require('gulp');
+var sass = require('gulp-sass')
+gulp.task('test',function () {
+    console.log('haha');
+});
+gulp.task('test2',function () {
+    console.log('hehe');
+});
+gulp.task('default',['test','test2']);
+
+gulp.task('test1',function () {
+    gulp.src('index.html');
+});
+gulp.task
